06dd17dcff2ffbd67b2539d66302109fe64bcedd
Dafazan <99418022+Dafazan@users.noreply.github.com>
2023-09-10 16:49:25 +0700

    link fixing

--- src/components/packagecard.jsx
+++ src/components/packagecard.jsx
@@ -3,121 +3,123 @@ import Image from "next/image";
 
 function PackageCard() {
     return (
-        <div className=' bg-white rounded-xl shadow-xl'>
-            <div className='p-3'>
-                <div className='flex'>
-                    <div className='w-8/12 font-semibold '>
-                        <h1>Umroh Dream Exclusive Plus Kereta Cepat</h1>
-                        <h2 className='text-2xl text-[#E3B02B] pt-2'>RP 39.5 Jt</h2>
-                    </div>
-                    <div className='w-4/12 bg-gray-400 rounded-xl'>
+        <a href="/package/detail">
+            <div className=' bg-white rounded-xl shadow-xl'>
+                <div className='p-3'>
+                    <div className='flex'>
+                        <div className='w-8/12 font-semibold '>
+                            <h1>Umroh Dream Exclusive Plus Kereta Cepat</h1>
+                            <h2 className='text-2xl text-[#E3B02B] pt-2'>RP 39.5 Jt</h2>
+                        </div>
+                        <div className='w-4/12 bg-gray-400 rounded-xl'>
 
-                    </div>
-                </div>
-                <div className=' py-3'>
-                    <div className='bg-[#E3B02B]'>bar Disini</div>
-                </div>
-                <div className='grid grid-cols-2 gap-1'>
-                    <div className='flex'>
-                        <div className='w-1/6'>
-                            <Image
-                                src='/assets/images/mdi_guest-room.png'
-                                width={25}
-                                height={25}
-                                alt="Picture of the author"
-                            />
-                        </div>
-                        <div>
-                            <p className='ps-1 text-black'>Satu kamar 4 Orang</p>
                         </div>
                     </div>
-                    <div className='flex'>
-                        <div className='w-1/6'>
-                            <Image
-                                src='/assets/images/mdi_guest-room.png'
-                                width={25}
-                                height={25}
-                                alt="Picture of the author"
-                            />
-                        </div>
-                        <div>
-                            <p className='ps-1 text-black'>10 Sep 2023</p>
-                        </div>
+                    <div className=' py-3'>
+                        <div className='bg-[#E3B02B]'>bar Disini</div>
                     </div>
-                    <div className='flex'>
-                        <div className='w-1/6'>
-                            <Image
-                                src='/assets/images/mdi_guest-room.png'
-                                width={25}
-                                height={25}
-                                alt="Picture of the author"
-                            />
-                        </div>
-                        <div>
-                            <p className='ps-1 text-black'>Garuda</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div className='w-1/6'>
-                            <Image
-                                src='/assets/images/mdi_guest-room.png'
-                                width={25}
-                                height={25}
-                                alt="Picture of the author"
-                            />
-                        </div>
-                        <div>
-                            <p className='ps-1 text-black'>Jakarta, +2 Kota</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div className='w-1/6'>
-                            <Image
-                                src='/assets/images/mdi_guest-room.png'
-                                width={25}
-                                height={25}
-                                alt="Picture of the author"
-                            />
-                        </div>
-                        <div>
-                            <p className='ps-1 text-black'>9 Hari</p>
-                        </div>
-                    </div>
-                    <div className='flex'>
-                        <div className='w-1/6'>
-                            <Image
-                                src='/assets/images/mdi_guest-room.png'
-                                width={25}
-                                height={25}
-                                alt="Picture of the author"
-                            />
-                        </div>
-                        <div>
-                            <p className='ps-1 text-black'>Hotel</p>
-
-                            <div class="ps-1 pt-2 flex items-center space-x-1">
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
+                    <div className='grid grid-cols-2 gap-1'>
+                        <div className='flex'>
+                            <div className='w-1/6'>
+                                <Image
+                                    src='/assets/images/mdi_guest-room.png'
+                                    width={25}
+                                    height={25}
+                                    alt="Picture of the author"
+                                />
                             </div>
+                            <div>
+                                <p className='ps-1 text-black'>Satu kamar 4 Orang</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div className='w-1/6'>
+                                <Image
+                                    src='/assets/images/mdi_guest-room.png'
+                                    width={25}
+                                    height={25}
+                                    alt="Picture of the author"
+                                />
+                            </div>
+                            <div>
+                                <p className='ps-1 text-black'>10 Sep 2023</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div className='w-1/6'>
+                                <Image
+                                    src='/assets/images/mdi_guest-room.png'
+                                    width={25}
+                                    height={25}
+                                    alt="Picture of the author"
+                                />
+                            </div>
+                            <div>
+                                <p className='ps-1 text-black'>Garuda</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div className='w-1/6'>
+                                <Image
+                                    src='/assets/images/mdi_guest-room.png'
+                                    width={25}
+                                    height={25}
+                                    alt="Picture of the author"
+                                />
+                            </div>
+                            <div>
+                                <p className='ps-1 text-black'>Jakarta, +2 Kota</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div className='w-1/6'>
+                                <Image
+                                    src='/assets/images/mdi_guest-room.png'
+                                    width={25}
+                                    height={25}
+                                    alt="Picture of the author"
+                                />
+                            </div>
+                            <div>
+                                <p className='ps-1 text-black'>9 Hari</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <div className='w-1/6'>
+                                <Image
+                                    src='/assets/images/mdi_guest-room.png'
+                                    width={25}
+                                    height={25}
+                                    alt="Picture of the author"
+                                />
+                            </div>
+                            <div>
+                                <p className='ps-1 text-black'>Hotel</p>
 
+                                <div class="ps-1 pt-2 flex items-center space-x-1">
+                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                    <svg class="w-4 h-4 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                    </svg>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </a>
     )
 }
 
