6feedb035ca6bf4a96199a53329ec1b4d353f8d9
TionoviarId <thenoviar@gmail.com>
2023-10-30 16:21:09 +0700

    membuat tabel

--- src/components/tabelHarga2.jsx
+++ src/components/tabelHarga2.jsx
@@ -3,26 +3,54 @@ import React from "react";
 function TabelHarga2() {
   return (
     <>
-      <div className="flex gap-16 justify-center items-center  border-2 border-black rounded-t-md">
-        <p className="">Tahap</p>
-        <p>Tanggal Pembayaran</p>
-        <p>Jumlah</p>
-      </div>
-      <div className="flex gap-16 justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
-          <p>Uang Muka</p>
-          <p>Bayar ke-2</p>
-          <p>Bayar ke-3</p>
+      <div className=" border-2  border-black rounded-lg">
+        <div className="flex gap-10 justify-center items-center  border-b-2 border-black rounded-t-md">
+          <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+            Tahap
+          </p>
+          <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+            Tanggal Pembayaran
+          </p>
+          <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+            Jumlah
+          </p>
         </div>
-        <div className="flex flex-col justify-center items-center">
-          <p>Uang Muka</p>
-          <p>Bayar ke-2</p>
-          <p>Bayar ke-3</p>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <p>Uang Muka</p>
-          <p>Bayar ke-2</p>
-          <p>Bayar ke-3</p>
+        <div className="flex  justify-center items-center">
+          <div className="flex flex-col justify-center items-center   w-[350px]">
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              Uang Muka
+            </p>
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              Bayar ke-2
+            </p>
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              Bayar ke-3
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center border-black border-x-2 w-full">
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              17 September 2023
+            </p>
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              17 September 2023
+            </p>
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              17 September 2023
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center  w-[350px] ">
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              Rp 6.000.000
+            </p>
+            <div className="">
+              <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+                Rp 6.000.000
+              </p>
+            </div>
+            <p className="p-3 font-semibold md:text-base sm:text-sm text-xs">
+              Rp 6.000.000
+            </p>
+          </div>
         </div>
       </div>
     </>
