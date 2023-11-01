e9232a9e9d6eed637da97f6c1371f34314164e6e
TionoviarId <thenoviar@gmail.com>
2023-11-01 16:36:56 +0700

    add key

--- src/components/datadiriFoam.jsx
+++ src/components/datadiriFoam.jsx
@@ -28,88 +28,91 @@ function DatadiriFoam({}) {
     setJamaah(deleteVal);
   };
   return (
-    <div>
-      <div className="bg-white md:w-10/12 w-11/12 md:ms-20 sm:ms-10 ms-7 mx-auto rounded-xl mt-10 shadow-2xl h-full">
-        {jamaah.map((val, i) => {
-          return (
-            <div className="w-full pt-7 font-semibold">
-              <div className="flex pb-1 px-14 gap-5">
-                <div>
-                  <img src="../../assets/vector/vectorya.svg" alt="" />
+    <>
+      <div>
+        <div className="bg-white md:w-10/12 w-11/12 md:ms-20 sm:ms-10 ms-7 mx-auto rounded-xl mt-10 shadow-2xl h-full">
+          {jamaah.map((val, i) => {
+            return (
+              <div key={i} className="w-full pt-7 font-semibold">
+                <div className="flex pb-1 px-14 gap-5">
+                  <div>
+                    <img src="../../assets/vector/vectorya.svg" alt="" />
+                  </div>
+                  <h1 className="text-2xl text-black pb-7">
+                    Jemaah Ke-
+                    {i + 1}
+                  </h1>
                 </div>
-                <h1 className="text-2xl text-black pb-7">
-                  Jemaah Ke-
-                  {i + 1}
-                </h1>
-              </div>
 
-              <div className=" flex  px-14 gap-5">
-                <div className="grid grid-cols-1">
-                  <p>Gender</p>
-                  <DropdownDetail />
-                  <br />
-                </div>
-                <div className="grid grid-cols-1">
-                  <div>
-                    <p>Nama Lengkap</p>
+                <div className=" md:flex  px-14 gap-5">
+                  <div className="grid grid-cols-1">
+                    <p>Gender</p>
+                    <DropdownDetail />
+                    <br />
                   </div>
-                  <TextInput
-                    id="nama"
-                    placeholder="Ketik Nama Lengkap.."
-                    required
-                    type="nama"
-                  />
+                  <div className="grid grid-cols-1 w-full">
+                    <div>
+                      <p>Nama Lengkap</p>
+                    </div>
+                    <TextInput
+                      id="nama"
+                      placeholder="Ketik Nama Lengkap.."
+                      required
+                      type="nama"
+                      className=""
+                    />
+                    <div>
+                      <p className="text-[#E3B02B] text-xs">
+                        &#40;Sesuai dengan KTP &#47; Paspor tanpa gelar&#41;
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className=" md:flex  px-14 gap-5 pt-5">
                   <div>
-                    <p className="text-[#E3B02B] text-xs">
-                      &#40;Sesuai dengan KTP &#47; Paspor tanpa gelar&#41;
-                    </p>
+                    <div>
+                      <p>Nomor Whatsapp</p>
+                    </div>
+                    <TextInput
+                      id="whatsapp"
+                      placeholder="Ketik Nomor Whatsapp.."
+                      required
+                      type="whatsapp"
+                      className=" "
+                    />
+                  </div>
+                  <div>
+                    <div>
+                      <p>Email</p>
+                    </div>
+                    <TextInput
+                      id="Email"
+                      placeholder="Ketik Email.."
+                      required
+                      type="Email"
+                      className=" "
+                    />
                   </div>
                 </div>
               </div>
-              <div className=" flex  px-14 gap-5 pt-5">
-                <div>
-                  <div>
-                    <p>Nomor Whatsapp</p>
-                  </div>
-                  <TextInput
-                    id="whatsapp"
-                    placeholder="Ketik Nomor Whatsapp.."
-                    required
-                    type="whatsapp"
-                    className=" "
-                  />
-                </div>
-                <div>
-                  <div>
-                    <p>Email</p>
-                  </div>
-                  <TextInput
-                    id="Email"
-                    placeholder="Ketik Email.."
-                    required
-                    type="Email"
-                    className=" "
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
 
-        <div className="flex justify-center items-center pb-20 pt-5">
-          <button onClick={handleClick} type="button">
-            <img src="../../assets/vector/VectorPlus.svg" alt="" />
-          </button>{" "}
-        </div>
-        <div className="pb-10 ">
-          <a href="/package/detail/bayar">
-            <div className="flex justify-center h-[60px] bg-[#E3B02B] hover:bg-[#fad26e] m-auto rounded-xl p-3 items-center w-11/12 ">
-              <p className="font-bold ">Lanjut Pembayaran</p>
-            </div>
-          </a>
+          <div className="flex justify-center items-center pb-20 pt-5">
+            <button onClick={handleClick} type="button">
+              <img src="../../assets/vector/VectorPlus.svg" alt="" />
+            </button>{" "}
+          </div>
+          <div className="pb-10 ">
+            <a href="/package/detail/bayar">
+              <div className="flex justify-center h-[60px] bg-[#E3B02B] hover:bg-[#fad26e] m-auto rounded-xl p-3 items-center w-11/12 ">
+                <p className="font-bold ">Lanjut Pembayaran</p>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
