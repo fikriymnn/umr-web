cc6b7535830351aed61130bb656f28b385934ea3
TionoviarId <thenoviar@gmail.com>
2023-11-01 16:37:22 +0700

    change name and logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,15 +6,21 @@ import Image from "next/image";
 export default function CustomNavbar() {
   return (
     <Navbar fluid rounded className="h-[80px]">
-      <Navbar.Brand href="/">
+      <Navbar.Brand href="/" className="ml-10">
         {/* <img
           alt="Flowbite React Logo"
           className="mr-3 h-6 sm:h-9"
           src="/favicon.svg"
         /> */}
-        <span className="self-center whitespace-nowrap text-3xl font-bold dark:text-white ml-10">
+        {/* <span className="self-center whitespace-nowrap text-3xl font-bold dark:text-white ml-10">
           UmrohYuk
-        </span>
+        </span> */}
+        <Image
+          src={"/assets/images/logo.png"}
+          alt=""
+          width={150}
+          height={150}
+        />
       </Navbar.Brand>
       <div className="flex md:order-2 mr-10">
         <a href="/login">
@@ -33,7 +39,7 @@ export default function CustomNavbar() {
           <div className="flex">
             <div className="w-2/12">
               <Image
-                src='/assets/images/Kaaba.png'
+                src="/assets/images/Kaaba.png"
                 width={50}
                 height={50}
                 alt="Picture of the author"
@@ -46,7 +52,7 @@ export default function CustomNavbar() {
           <div className="flex">
             <div className="w-2/12 ">
               <Image
-                src='/assets/images/InfoSquared.png'
+                src="/assets/images/InfoSquared.png"
                 width={50}
                 height={50}
                 alt="Picture of the author"
@@ -56,10 +62,10 @@ export default function CustomNavbar() {
           </div>
         </Navbar.Link>
         <Navbar.Link href="/more">
-          <div className="flex">
-            <div className="w-2/12">
+          <div className="flex gap-1">
+            <div className="w-2/12 my-auto">
               <Image
-                src='/assets/images/grid.png'
+                src="/assets/images/grid.png"
                 width={50}
                 height={50}
                 alt="Picture of the author"
@@ -68,15 +74,14 @@ export default function CustomNavbar() {
             <p>Lainnya</p>
           </div>
         </Navbar.Link>
-        <Navbar.Link href="/search">
+        {/* <Navbar.Link href="/search">
           <Image
-            src='/assets/vector/magnifier.svg'
-            alt='icon'
+            src="/assets/vector/magnifier.svg"
+            alt="icon"
             width={25}
             height={25}
           />
-        </Navbar.Link>
-
+        </Navbar.Link> */}
       </Navbar.Collapse>
     </Navbar>
   );
