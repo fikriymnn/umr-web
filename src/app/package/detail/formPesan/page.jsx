eb7b84d1b432612a6c33c2753fe1c8e2463069fa
TionoviarId <thenoviar@gmail.com>
2023-11-01 16:38:03 +0700

    add responsive and add frontend features

--- src/app/package/detail/formPesan/page.jsx
+++ src/app/package/detail/formPesan/page.jsx
@@ -19,49 +19,53 @@ const steps = [
 ];
 function FormPesan() {
   return (
-    <div className="bg">
-      <div className="flex flex-col pt-10 bg-left bg-contain bg-[url('/assets/images/image2.png')] z-10">
-        <div className="flex mx-auto gap-4 justify-between">
-          <div className="flex gap-1">
-            <p className="rounded-full border-2 border-amber-400 text-amber-400 w-10 text-center ">
-              1
-            </p>
-            <p className="rounded-full text-amber-400 w-full ">Data Pesanan</p>
-          </div>
-          <div className="w-12 h-1 bg-zinc-300 my-auto mx-auto rounded-full "></div>
-          <div className="flex gap-1">
-            <p className="rounded-full border-2 border-zinc-300 text-zinc-300 w-10 text-center ">
-              2
-            </p>
-            <p className="rounded-full text-zinc-300 w-full ">Bayar</p>
-          </div>
-        </div>
-        <div className="md:w-6/12  ">
-          <section>
-            <NamaCustomer />
-          </section>
-          <section>
-            <TipePembayaranFoam />
-          </section>
-          <section>
-            <DatadiriFoam />
-          </section>
-          <section>
-            <div className="md:px-14 w-10/12 md:ms-10 mb-20 h-96 mt-5 ml-10 md:text-base sm:text-sm text-xs">
-              {" "}
-              <p className="font-bold">
-                Jika anda setuju dengan{" "}
-                <span className="text-[#E3B02B]">
-                  Syarat & Kebijakan Privasi,
-                </span>{" "}
-                Silahkan klik tombol &rdquo;Lanjut Pembayaran&rdquo; . untuk
-                memilih cara pembayaran
+    <>
+      <div className="bg">
+        <div className="flex flex-col pt-10 bg-left bg-contain bg-[url('/assets/images/image2.png')] z-10">
+          <div className="flex mx-auto gap-4 justify-between">
+            <div className="flex gap-1">
+              <p className="rounded-full border-2 border-amber-400 text-amber-400 w-10 text-center ">
+                1
+              </p>
+              <p className="rounded-full text-amber-400 w-full ">
+                Data Pesanan
               </p>
             </div>
-          </section>
+            <div className="w-12 h-1 bg-zinc-300 my-auto mx-auto rounded-full "></div>
+            <div className="flex gap-1">
+              <p className="rounded-full border-2 border-zinc-300 text-zinc-300 w-10 text-center ">
+                2
+              </p>
+              <p className="rounded-full text-zinc-300 w-full ">Bayar</p>
+            </div>
+          </div>
+          <div className="md:w-6/12  ">
+            <section>
+              <NamaCustomer />
+            </section>
+            <section>
+              <TipePembayaranFoam />
+            </section>
+            <section>
+              <DatadiriFoam />
+            </section>
+            <section>
+              <div className="md:px-14 w-10/12 md:ms-10 mb-20 h-96 mt-5 ml-10 md:text-base sm:text-sm text-xs">
+                {" "}
+                <p className="font-bold">
+                  Jika anda setuju dengan{" "}
+                  <span className="text-[#E3B02B]">
+                    Syarat & Kebijakan Privasi,
+                  </span>{" "}
+                  Silahkan klik tombol &rdquo;Lanjut Pembayaran&rdquo; . untuk
+                  memilih cara pembayaran
+                </p>
+              </div>
+            </section>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/packagecard.jsx
+++ src/components/packagecard.jsx
@@ -14,16 +14,24 @@ function PackageCard() {
     <a href="/package/detail">
       <div className=" bg-white rounded-xl shadow-xl hover:translate-y-[-15px] duration-100 ease-in-out">
         <div className="p-3">
-          <div className="flex">
-            <div className="w-8/12  ">
-              <h1 className="md:text-[17px] sm:text-[15px] font-semibold">
+          <div className="md:flex gap-1">
+            <div className="md:w-8/12  ">
+              <h1 className="md:text-[17px] sm:text-[15px] text-[17px] font-semibold">
                 Umroh Dream Exclusive Plus Kereta Cepat
               </h1>
               <h2 className="md:text-[28px] sm:text-[24px] text-[28px] font-semibold  text-[#E3B02B] pt-2">
                 RP 39.5 Jt
               </h2>
             </div>
-            <div className="w-4/12 h-24 bg-gray-400 rounded-xl"></div>
+            <div className="md:w-4/12 w-full sm:w-full sm:h-full md:h-full h-full mx-auto rounded-xl">
+              <Image
+                src={"/assets/images/haji.jpg"}
+                width={400}
+                height={95}
+                alt=""
+                className="rounded-lg"
+              />
+            </div>
           </div>
           <div className=" py-3">
             <div className="flex items-center justify-center">
@@ -126,9 +134,9 @@ function PackageCard() {
               <div>
                 <p className="ps-1 text-black md:text-base sm:text-xs">Hotel</p>
 
-                <div class="ps-1 pt-2 flex items-center space-x-1">
+                <div className="ps-1 pt-2 flex items-center space-x-1">
                   <svg
-                    class="w-4 h-4 text-yellow-300"
+                    className="w-4 h-4 text-yellow-300"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -137,7 +145,7 @@ function PackageCard() {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    class="w-4 h-4 text-yellow-300"
+                    className="w-4 h-4 text-yellow-300"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -146,7 +154,7 @@ function PackageCard() {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    class="w-4 h-4 text-yellow-300"
+                    className="w-4 h-4 text-yellow-300"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -155,7 +163,7 @@ function PackageCard() {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    class="w-4 h-4 text-yellow-300"
+                    className="w-4 h-4 text-yellow-300"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -164,7 +172,7 @@ function PackageCard() {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    class="w-4 h-4 text-gray-300 dark:text-gray-500"
+                    className="w-4 h-4 text-gray-300 dark:text-gray-500"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
