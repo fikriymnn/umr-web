3c3a9bcefe8ffd21a301f11d546c260da9f838a7
TionoviarId <thenoviar@gmail.com>
2023-11-08 15:52:16 +0700

    memindahkan content ke components

--- src/components/percayakan_mobile.jsx
+++ src/components/percayakan_mobile.jsx
@@ -1,19 +1,20 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import { isMobile } from "react-device-detect";
 
 function Percayakan_mobile() {
-    return (
-        <>
-            {isMobile && (
-                <div className="w-4/5 mx-auto  mt-16">
-                    <p className="font-extrabold text-3xl md:hidden sm:visible visible">
-                        Percayakan Perjalanan Umroh anda kepada Ibadahku
-                    </p>
-                </div>
-            )}
-        </>
-    )
+  return (
+    <>
+      {isMobile && (
+        <div className="w-4/5 mx-auto  mt-16">
+          <p className="font-extrabold text-2xl md:hidden sm:visible visible">
+            Percayakan Perjalanan Umroh anda kepada{" "}
+            <span className="text-primary">Ibadahku</span>
+          </p>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Percayakan_mobile
+export default Percayakan_mobile;
