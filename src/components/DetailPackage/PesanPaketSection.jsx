d2dcad4d638b9b435f6f0385426a1fe22f5743ac
TionoviarId <thenoviar@gmail.com>
2023-11-23 16:35:06 +0700

    ukuran

--- src/components/DetailPackage/PesanPaketSection.jsx
+++ src/components/DetailPackage/PesanPaketSection.jsx
@@ -21,13 +21,13 @@ function PesanPaketSection() {
           </h2>
         </div>
         <div className="mt-3 mx-auto">
-          <div className="w-full  h-7 rounded-xl border-2 relative border-black">
-            <div className=" w-full absolute flex items-center justify-center text-center font-semibold md:text-base sm:text-base text-xs my-auto">
-              <p className="my-auto">Tersisa {numero.available} Paket</p>
+          <div className="w-full md:h-7 h-6 rounded-md border md:border-2 relative border-black">
+            <div className="md:w-full sm:w-full w-full h-full absolute flex items-center justify-center text-center font-semibold md:text-sm sm:text-xs text-xs ">
+              <p>Tersisa {numero.available} Paket</p>
             </div>
             <div
               style={{ width: availability }}
-              className="h-full bg-[#be9223] rounded-xl"
+              className="h-full bg-[#ffc73a] rounded-[4px]"
             ></div>
           </div>
         </div>
