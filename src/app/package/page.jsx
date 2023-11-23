3dc46dcb649393577fe97f09678c191b423f8edc
TionoviarId <thenoviar@gmail.com>
2023-11-23 16:36:06 +0700

    memperbaiki ukuran

--- src/app/package/page.jsx
+++ src/app/package/page.jsx
@@ -6,24 +6,27 @@ import DefaultCarousel from "@/components/Carousel";
 import PackageCard from "@/components/packagecard";
 import FilterNotMobilePackage from "@/components/FilterNotMobilePackage"
 import FilterIsMobilePackage from "@/components/FilterIsMobilePackage"
+import FilterPackages from "@/components/FilterPackages"
 function Package() {
 
   return (
     <div className="bg pb-10">
-      <div className="flex  pt-10 bg-cover  bg-[url('/assets/images/image2.png')] z-10 relative">
+      <FilterPackages />
+      <div className='h-[40px]'></div>
+      <div className="flex  pt-5 bg-cover  bg-[url('/assets/images/image2.png')] z-10 relative">
         {/* <FilterIsMobilePackage /> */}
         <FilterNotMobilePackage />
         <div className="md:w-8/12 w-full mx-auto md:ml-10 md:mr-20 md:p-0 px-5">
           <div className="bg-white shadow-xl rounded-xl md:w-10/12 w-full">
             <div className="flex md:flex-row flex-col">
               <div className="md:w-3/6 w-full p-5 py-3">
-                <h1 className="w-full mb-2 text-xl font-semibold">
+                <h1 className="w-full mb-2 text-sm md:text-xl font-semibold">
                   Jenis Keberangkatan
                 </h1>
                 <Dropdown />
               </div>
               <div className="md:w-3/6 w-full p-5 md:py-3 pt-0">
-                <h1 className="w-full mb-2 text-xl font-semibold">
+                <h1 className="w-full mb-2 text-sm md:text-xl font-semibold">
                   Waktu Keberangkatan
                 </h1>
                 <Dropdown />
@@ -34,7 +37,7 @@ function Package() {
             <h1 className="text-xl font-semibold  my-5">
               List Paket Umroh Yang Tersedia
             </h1>
-            <div className="mahfud md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-10">
+            <div className="mahfud md:grid-cols-2 sm:grid-cols-2 grid-cols-2 md:gap-10 gap-2">
               <PackageCard />
               <PackageCard />
               <PackageCard />
