d27ed8a1b2cb2d8bfa465c914572b5817db67492
Dafazan <dafazanlbs@gmail.com>
2023-12-11 17:06:02 +0700

    color and size modified

--- src/components/Homepage/percayakan.jsx
+++ src/components/Homepage/percayakan.jsx
@@ -15,64 +15,64 @@ function Percayakan() {
           </div>
         )}
         <div className="grid grid-cols-1 md:gap-7 gap-3 md:mt-0 sm:mt-5 mt-5">
-          <div className=" flex w-10/12 py-3 px-1 bg-[#959595] mx-auto rounded-xl">
+          <div className=" flex shadow-md w-10/12 py-3 px-1 bg-[#ffffff] mx-auto rounded-xl shadow-md">
             <div className="flex px-1">
-              <div className=" bg-[#959595]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
+              <div className=" bg-[#ffffff]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
                 <div className="bg-primary h-16 w-16 rounded-xl  "></div>
               </div>
               <div className="grid grid-cols-1  ps-2">
-                <p className="text-white  md:text-base sm:text-base text-xs font-bold pt-2">
+                <p className="text-black  md:text-base sm:text-base text-xs font-bold pt-2">
                   Lorem ipsum Dolor sit Amet
                 </p>
-                <p className="text-white md:text-base sm:text-base text-xs">
+                <p className="text-black md:text-base sm:text-base text-xs">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
                   quia a dicta molestias sed
                 </p>
               </div>
             </div>
           </div>
-          <div className=" flex w-10/12 py-3 px-1 bg-[#959595] mx-auto rounded-xl">
+          <div className=" flex shadow-md w-10/12 py-3 px-1 bg-[#ffffff] mx-auto rounded-xl">
             <div className="flex px-1">
-              <div className=" bg-[#959595]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
+              <div className=" bg-[#ffffff]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
                 <div className="bg-primary h-16 w-16 rounded-xl  "></div>
               </div>
               <div className="grid grid-cols-1  ps-2">
-                <p className="text-white  md:text-base sm:text-base text-xs font-bold pt-2">
+                <p className="text-black  md:text-base sm:text-base text-xs font-bold pt-2">
                   Lorem ipsum Dolor sit Amet
                 </p>
-                <p className="text-white md:text-base sm:text-base text-xs">
+                <p className="text-black md:text-base sm:text-base text-xs">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
                   quia a dicta molestias sed
                 </p>
               </div>
             </div>
           </div>
-          <div className=" flex w-10/12 py-3 px-1 bg-[#959595] mx-auto rounded-xl">
+          <div className=" flex shadow-md w-10/12 py-3 px-1 bg-[#ffffff] mx-auto rounded-xl">
             <div className="flex px-1">
-              <div className=" bg-[#959595]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
+              <div className=" bg-[#ffffff]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
                 <div className="bg-primary h-16 w-16 rounded-xl  "></div>
               </div>
               <div className="grid grid-cols-1  ps-2">
-                <p className="text-white  md:text-base sm:text-base text-xs font-bold pt-2">
+                <p className="text-black  md:text-base sm:text-base text-xs font-bold pt-2">
                   Lorem ipsum Dolor sit Amet
                 </p>
-                <p className="text-white md:text-base sm:text-base text-xs">
+                <p className="text-black md:text-base sm:text-base text-xs">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
                   quia a dicta molestias sed
                 </p>
               </div>
             </div>
           </div>
-          <div className=" flex w-10/12 py-3 px-1 bg-[#959595] mx-auto rounded-xl">
+          <div className=" flex shadow-md w-10/12 py-3 px-1 bg-[#ffffff] mx-auto rounded-xl">
             <div className="flex px-1">
-              <div className=" bg-[#959595]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
+              <div className=" bg-[#ffffff]  grid place-content-center rounded-xl lg:px-7 md:px-1 ">
                 <div className="bg-primary h-16 w-16 rounded-xl  "></div>
               </div>
               <div className="grid grid-cols-1  ps-2">
-                <p className="text-white  md:text-base sm:text-base text-xs font-bold pt-2">
+                <p className="text-black  md:text-base sm:text-base text-xs font-bold pt-2">
                   Lorem ipsum Dolor sit Amet
                 </p>
-                <p className="text-white md:text-base sm:text-base text-xs">
+                <p className="text-black md:text-base sm:text-base text-xs">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
                   quia a dicta molestias sed
                 </p>
