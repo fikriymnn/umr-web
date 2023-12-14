dcdaf1a6c19e5480c89ef8264eac7aef474ddf3b
acepW <acepwahyu001@gmail.com>
2023-12-14 16:47:32 +0700

    mengubah layout

--- src/app/mitraKami/page.jsx
+++ src/app/mitraKami/page.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import MitraCard from '@/components/mitraCard'
+import MitraCard from "@/components/mitraCard";
 
 function MitraKami() {
-    return (
-        <>
-
-            <div> <p className=" text-black text-xl font-bold md:px-32 py-14 p-7">
-                Mitra Kami
-            </p></div>
-            <div className='grid md:grid-cols-2 grid-cols-1 gap-5 md:mx-32 mx-7 pb-10'>
-                <MitraCard />
-                <MitraCard />
-                <MitraCard />
-                <MitraCard />
-                <MitraCard />
-                <MitraCard />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        {" "}
+        <p className=" text-black text-xl font-bold  py-14 lg:mx-32 md:mx-10  sm:mx-5 mx-7">
+          Mitra Kami
+        </p>
+      </div>
+      <div className="grid xl:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-5 lg:mx-32 md:mx-10  sm:mx-5 mx-7 pb-10">
+        <MitraCard />
+        <MitraCard />
+        <MitraCard />
+        <MitraCard />
+        <MitraCard />
+        <MitraCard />
+      </div>
+    </>
+  );
 }
 
-export default MitraKami
+export default MitraKami;
