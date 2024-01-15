d30d9e5bbd0892670c0b42f92358e0294b42a685
TionoviarId <thenoviar@gmail.com>
2024-01-15 17:02:36 +0700

    mitra

--- src/app/mitraKami/page.jsx
+++ src/app/mitraKami/page.jsx
@@ -1,8 +1,22 @@
 import React from "react";
-
+import axios from "axios";
 import MitraCard from "@/components/mitraCard";
+async function GetDataMitra() {
+  let data;
+  try {
+    const res = await axios.get(
+      "http://localhost:5000/api/mitra?skip=0&limit=9"
+    );
+    data = res.data.data;
 
-function MitraKami() {
+
+  } catch (error) {
+    data = null;
+  }
+  return data;
+}
+async function MitraKami() {
+  const DataMitra = await GetDataMitra();
   return (
     <>
       <div className="bg ">
@@ -16,13 +30,21 @@ function MitraKami() {
                 Mitra Kami
               </p>
             </div>
-            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 sm:grid-cols-2 gap-3  pb-10 xl:p-0 lg:p-5 px-3">
-              <MitraCard />
-              <MitraCard />
-              <MitraCard />
-              <MitraCard />
-              <MitraCard />
-              <MitraCard />
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 sm:grid-cols-2 gap-3  pb-10 xl:p-0 lg:p-5 px-3 mb-20">
+              {DataMitra == null ? (
+                <div key={index}>skksks</div>
+              ) : (
+                DataMitra.map((data, index) => {
+
+                  return (
+                    <>
+
+                      <MitraCard namaTravel={data.nama_mitra}
+                        key={index}
+                        noIzin={data.no_izin_umroh} penilaian={"data.rating.length"} ratingNilai={data.rating.toFixed(1)} profil={`http://localhost:5000/images/${data.foto_profil}`} />
+                    </>
+                  )
+                }))}
             </div>
           </div>
         </div>
